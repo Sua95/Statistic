5f2a0295ba68456ea36eecec7229c717e6fa82ec
Amaury Jara <amaurysuajg@gmail.com>
2023-01-01 21:47:42 -0600

    mean function added

--- statistics_course.py
+++ statistics_course.py
@@ -5,3 +5,16 @@ Created on Sun Jan  1 21:17:45 2023
 @author: Sua
 """
 
+sample_list = [15, 5]
+
+def mean(sample_list):
+    sum_of_sample = 0
+    len_sample = len(sample_list)
+    for i in sample_list:
+        sum_of_sample += i
+    return sum_of_sample/len_sample
+    
+
+
+print(mean(sample_list))
+        
