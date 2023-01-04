cf59a6c86c3f1e42cfa5ecaf664dcdb9b47a0a99
Sua95 <amaurysuajg@gmail.com>
2023-01-03 20:06:42 -0600

    stdev and variance added from statistic

--- statistics_course.py
+++ statistics_course.py
@@ -5,10 +5,11 @@ Created on Sun Jan  1 21:17:45 2023
 @author: Sua
 """
 from collections import Counter
+import statistics
+import inspect
 
 
-sample_list = [5 ,10, 9, 10, 9, 8, 10, 5, 7, 6, 9, 7, 8, 8, 8, 7]
-dictionary_list = {'arrow' : 1, 'sword': 2, 'shield' : 3}
+sample_list = [5 ,10, 9, 10, 9, 8]
 
 def mean(sample_list):
     mean = sum(sample_list) / len(sample_list)
@@ -25,21 +26,14 @@ def mode(sample_list):
     sorted_list = Counter(sorted(sample_list))
     mode = sorted_list.most_common(1)
     return mode
-    
+
 def variance(sample_list):
-    mean = sum(sample_list) / len(sample_list)
-    squared_differences = [(x - mean)**2 for x in sample_list]
-    variance = sum(squared_differences) / len(sample_list)
+    variance = statistics.variance(sample_list)
     return variance
-    
+
 def stdev(sample_list):
-    mean = sum(sample_list) / len(sample_list)
-    squared_differences = [(x - mean)**2 for x in sample_list]
-    variance = sum(squared_differences) / len(sample_list)
-    stdev = variance // 2
+    stdev = statistics.stdev(sample_list)
     return stdev
-        
-    
-    
-print(variance(sample_list))    
+
 print(stdev(sample_list))
+print(variance(sample_list))
