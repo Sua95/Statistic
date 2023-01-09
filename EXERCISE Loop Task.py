25e287da039d63eaa4855d7238a578c9be007d55
Amaury Jara <amaurysuajg@gmail.com>
2023-01-08 22:34:45 -0600

    all excercises solved and comments added in EXCERCISE Loop Task.py

--- EXERCISE Loop Task.py
+++ EXERCISE Loop Task.py
@@ -24,7 +24,7 @@ def countin_difference(num_one, num_two):
             print(num_two)
             num_two += 1
 
-
+# Two if statements who compare who is the bigger number and print the differences one by one
 
 '''
 Question 2
@@ -38,7 +38,7 @@ def print_backwards(user_string):
     for l in range(len(user_string)-1, -1, -1):
         print(user_string[l])
     
-
+# With the triple -1 it's equal to say reversed
 
 '''
 Question 3
@@ -54,6 +54,8 @@ def multiplications_w_input():
         mul_array.append(user_num*(i+1))
         print(f'{user_num} by {i+1} equals {user_num * (i + 1)}')
         
+# The print it's inside the loop to print on every loop
+
 
 '''
 Question 4
@@ -70,6 +72,7 @@ def times_table(multiplicator, number):
         for num in number:
             print(f'{num} x {mult} = {num * mult}', end=" ")
             
+# A loop inside another loop to print all times table from 1 to 12
 
 '''
 Question 5
@@ -77,12 +80,13 @@ Ask the user to input a sequence of numbers. Then calculate the mean
 and print the result
 '''
 
-user_array = []
-
-for i in range(5):
-    user_array.append(int(input('Choose 5 numbers: ')))
+def mean_input():
     
-print(f'the mean is: {sum(user_array) / len(user_array)}')
+# An input inside a loop then the mean formula
+    user_array = []
+    for i in range(5):
+        user_array.append(int(input('Choose 5 numbers: ')))
+        print(f'the mean is: {sum(user_array) / len(user_array)}')
 
 
 '''
@@ -92,6 +96,16 @@ positive ints up to a given number. e.g 5 factorial is 5x4x3x2x1)
 '''
 
 
+number = 4
+    
+def factorial(number):
+    result = 1
+    for i in range(1, number + 1):
+        result *= i
+    return result
+        
+# i never gets 0 cause range starts in 1
+
 '''
 Question 7
 Write code to calculate Fibonacci numbers. Create list containing
@@ -100,6 +114,15 @@ two. Series starts 0 1 1 2 3 5 8 13 ....)
 '''
 
 
+fibonacci = [0, 1]
+
+def fibonacci_autofill(initial_array):
+    
+# append new values with the sum of the last 2 numbers in the array
+    for i in range(19):
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+        
+
 
 '''
 Question 8
@@ -107,6 +130,11 @@ The previous question was the first to contain comments. Go back
 through the other questions in this file and add comments to the
 solutions.
 '''
+
+
+# DONE
+
+
 
 '''
 Question 9
@@ -120,7 +148,15 @@ Question 9
 Can you draw this using python? (comment the solution code)
 '''
 
+def f_in_prints():
+    print("*****")
+    print("*")
+    print("****")
+    print("*")
+    print("*")
+    print("*")
 
+# Nothing to comment
 
 '''
 Question 10
@@ -128,3 +164,45 @@ Write some code that will determine all odd and even numbers
 between 1 and 100. Put the odds in a list named odd and the evens
 in a list named even.
 '''
+
+even_list = []
+odd_list = []
+
+
+for n in range(1,101):
+    if n % 2 == 0:
+        even_list.append(n)
+    else:
+        odd_list.append(n)
+        
+print(even_list)
+print(odd_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
